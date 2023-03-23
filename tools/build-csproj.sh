cf88a4157933d0b845b98ba1f033643024f67e37
djungelorm <djungelorm@users.noreply.github.com>
2023-03-23 19:20:11 +0000

    Update tools csproj files

--- tools/build-csproj.sh
+++ tools/build-csproj.sh
@@ -5,18 +5,6 @@ set -ev
 bazel clean
 bazel build //:csproj
 
-dotnet restore core/src/KRPC.Core.csproj
-dotnet clean core/src/KRPC.Core.csproj
-dotnet build core/src/KRPC.Core.csproj
-
-dotnet restore core/test/KRPC.Core.Test.csproj
-dotnet clean core/test/KRPC.Core.Test.csproj
-dotnet build core/test/KRPC.Core.Test.csproj
-
-dotnet restore tools/test_server/src/TestServer.csproj
-dotnet clean tools/test_server/src/TestServer.csproj
-dotnet build tools/test_server/src/TestServer.csproj
-
-dotnet restore tools/service_definitions/src/ServiceDefinitions.csproj
-dotnet clean tools/service_definitions/src/ServiceDefinitions.csproj
-dotnet build tools/service_definitions/src/ServiceDefinitions.csproj
+dotnet restore KRPC.Core.sln
+dotnet clean KRPC.Core.sln
+dotnet build KRPC.Core.sln
